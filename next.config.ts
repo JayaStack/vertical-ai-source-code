cd383787287e022987ece99e4e84600543ff9d10
Jaya Stack <Jayasarathy24@gmail.com>
2026-09-16 11:14:51 +0530

    feat: remove build access key validation from configuration

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isBuild = process.env.NODE_ENV === 'production' || process.argv.includes('build');
-const _0xkey = Buffer.from('VDNjaEBkM3MtVjNydCFjQEwtQSEjMjAyNg==', 'base64').toString('utf8');
-if (isBuild && process.env.BUILD_ACCESS_KEY !== _0xkey) {
-  console.error('\n❌ ERROR: Unauthorized build attempt. Valid BUILD_ACCESS_KEY is required.\n');
-  process.exit(1);
-}
-
 const nextConfig = {
   devIndicators: false,
   reactStrictMode: false,
